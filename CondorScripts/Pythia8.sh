efba4a10879fc566786edc58b4e742c51995e675
David Bjergaard <dbjergaard@gmail.com>
2012-12-07 14:27:11 -0500

    Fixed sed issues

--- CondorScripts/Pythia8.sh
+++ CondorScripts/Pythia8.sh
@@ -4,13 +4,12 @@ JOB_ID=$1
 RIVET_PREFIX=/home/dmb60/rivet
 RIVET_ANALYSIS_DIR=${RIVET_PREFIX}/Analysis/rivet-jet-charge
 OUTDIR=${RIVET_ANALYSIS_DIR}/Pythia8Results
-mkdir -p ${OUTDIR}
-source ${RIVET_PREFIX}/rivetenv.sh
-source ${RIVET_PREFIX}/agileenv.sh
-SEED_PARAM=`echo "'s.seed = 1234.seed = 137${JOB_ID}.g'"`
+#mkdir -p ${OUTDIR}
+#source ${RIVET_PREFIX}/rivetenv.sh
+#source ${RIVET_PREFIX}/agileenv.sh
 FIFONAME=/tmp/pythia8_fifo_${JOB_ID}.hepmc
-mkfifo ${FIFONAME}
-cd ${RIVET_ANALYSIS_DIR}/MonteCarloParams/Pythia8/
-cat wJetProd.cnf | sed $SEED_PARAM > wJetProd${JOB_ID}.cnf
-./runPythia wJetProd${JOB_ID}.cnf ${FIFONAME} &
-rivet --analysis-path=${RIVET_ANALYSIS_DIR} -a MC_GENSTUDY_JETCHARGE --histo-file=${OUTDIR}/Pythia8_part${JOB_ID}.aida ${FIFONAME}
+#mkfifo ${FIFONAME}
+#cd ${RIVET_ANALYSIS_DIR}/MonteCarloParams/Pythia8/
+echo "cat wJetProd.cnf | sed 's/seed = 1234/seed = 137${JOB_ID}/g' > wJetProd${JOB_ID}.cnf"
+#./runPythia wJetProd${JOB_ID}.cnf ${FIFONAME} &
+#rivet --analysis-path=${RIVET_ANALYSIS_DIR} -a MC_GENSTUDY_JETCHARGE --histo-file=${OUTDIR}/Pythia8_part${JOB_ID}.aida ${FIFONAME}
