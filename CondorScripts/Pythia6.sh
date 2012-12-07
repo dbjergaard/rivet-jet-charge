2e52d6c1896f8d0eda8674a0ea029751578d7453
David Bjergaard <dbjergaard@gmail.com>
2012-12-07 13:22:01 -0500

    Fixed bugs

--- CondorScripts/Pythia6.sh
+++ CondorScripts/Pythia6.sh
@@ -7,8 +7,11 @@ OUTDIR=${RIVET_ANALYSIS_DIR}/Pythia6Results
 mkdir -p ${OUTDIR}
 source ${RIVET_PREFIX}/rivetenv.sh
 source ${RIVET_PREFIX}/agileenv.sh
-export AGILE_GEN_PATH=${RIVET_PREFIX}/local/generators
+
 cd ${RIVET_ANALYSIS_DIR}
 mkfifo fifo_${JOB_ID}.hepmc
-agile-runmc Pythia6:426 --beams=LHC:7000 -n 25000 -P MonteCarloParams/Pythia6/wInclusiveProduction.pars -p 'MRPY(1)=100788$1' {-o fifo_${JOB_ID}.hepmc &
-rivet --analysis-path=${RIVET_ANALYSIS_DIR} -a MC_GENSTUDY_JETCHARGE --histo-file=${OUTDIR}/Pythia6_part${JOB_ID}.aida
+AGILE_GEN_PATH=${RIVET_PREFIX}/local/generators \
+    agile-runmc Pythia6:426 --beams=LHC:7000 -n 25000 \
+    -P MonteCarloParams/Pythia6/wInclusiveProduction.pars -p 'MRPY(1)=100788$1' \
+    -o fifo_${JOB_ID}.hepmc &
+rivet --analysis-path=${RIVET_ANALYSIS_DIR} -a MC_GENSTUDY_JETCHARGE --histo-file=${OUTDIR}/Pythia6_part${JOB_ID}.aida fifo_${JOB_ID}.hepmc
