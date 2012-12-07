ef2927c6c5eab3d97e5b292dec93f6bf41bd15f8
David Bjergaard <dbjergaard@gmail.com>
2012-12-07 16:20:05 -0500

    Tweaked herwig script

--- CondorScripts/Herwig++.sh
+++ CondorScripts/Herwig++.sh
@@ -6,5 +6,6 @@ RIVET_ANALYSIS_DIR=${RIVET_PREFIX}/Analysis/rivet-jet-charge
 source ${RIVET_PREFIX}/rivetenv.sh
 source ${RIVET_PREFIX}/agileenv.sh
 cd ${RIVET_ANALYSIS_DIR}/MonteCarloParams/Herwig++/
-Herwig++ run -N 25000 -s 1370${JOB_ID} --tag='run${JOB_ID}' LHC.run
+export RIVET_ANALYSIS_PATH=${RIVET_ANALYSIS_DIR}
+Herwig++ run -N 25000 -s 1370${JOB_ID} --tag=`echo "'run${JOB_ID}'"` LHC.run
 
