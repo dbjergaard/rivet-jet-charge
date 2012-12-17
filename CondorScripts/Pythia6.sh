41c3586c0264dc1116a12a54d5472f217a176f62
David Bjergaard <dbjergaard@gmail.com>
2012-12-17 15:14:55 -0500

    Prepare condor scripts for Batch job (Sherpa, Herwig++, Pythia6 NOCR and Perugia Tune, Pythia8)

--- CondorScripts/Pythia6.sh
+++ CondorScripts/Pythia6.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -l
 
 JOB_ID=$1
+TUNE=$2
 RIVET_PREFIX=${HOME}/rivet
 RIVET_ANALYSIS_DIR=${RIVET_PREFIX}/Analysis/rivet-jet-charge
 OUTDIR=${RIVET_ANALYSIS_DIR}/HighStatsAida/Pythia6
@@ -9,13 +10,15 @@ export AGILE_GEN_PATH=${RIVET_PREFIX}/local/generators
 mkdir -p ${OUTDIR}
 source ${RIVET_PREFIX}/rivetenv.sh
 source ${RIVET_PREFIX}/agileenv.sh
+# do I need to add the TUNE code to the seed?
 SEED_PARAM=`echo "MRPY(1)=1370${JOB_ID}"`
 cd ${RIVET_ANALYSIS_DIR}
-FIFONAME=/tmp/pythia6_fifo_${JOB_ID}.hepmc
+FIFONAME=/tmp/pythia6_fifo_${TUNE}_${JOB_ID}.hepmc
 mkfifo ${FIFONAME}
 agile-runmc Pythia6:426 --beams=LHC:7000 -n 25000 \
-    -P MonteCarloParams/Pythia6/wInclusiveProduction.pars -p ${SEED_PARAM} \
-    -o ${FIFONAME} &
+    -P MonteCarloParams/Pythia6/wInclusiveProduction.pars \
+    -p PYTUNE=${TUNE} -p ${SEED_PARAM} -o ${FIFONAME} &
 sleep 1 #let things settle after starting AGILe
-rivet --analysis-path=${RIVET_ANALYSIS_DIR} -a MC_GENSTUDY_JETCHARGE --histo-file=${OUTDIR}/Pythia6_part${JOB_ID}.aida ${FIFONAME}
+rivet --analysis-path=${RIVET_ANALYSIS_DIR} -a MC_GENSTUDY_JETCHARGE \
+      --histo-file=${OUTDIR}/Pythia6_tune${TUNE}_part${JOB_ID}.aida ${FIFONAME}
 rm ${FIFONAME}
--- CondorScripts/Sherpa.sh
+++ CondorScripts/Sherpa.sh
@@ -3,9 +3,11 @@
 JOB_ID=$1
 RIVET_PREFIX=${HOME}/rivet
 RIVET_ANALYSIS_DIR=${RIVET_PREFIX}/Analysis/rivet-jet-charge
+OUTDIR=${RIVET_ANALYSIS_DIR}/HighStatsAida/Sherpa/part${JOB_ID}
+mkdir -p $OUTDIR
 source ${RIVET_PREFIX}/rivetenv.sh
 source ${RIVET_PREFIX}/agileenv.sh
-cd ${RIVET_ANALYSIS_DIR}/MonteCarloParams/Herwig++/
+cd ${RIVET_ANALYSIS_DIR}/MonteCarloParams/Sherpa/
 export RIVET_ANALYSIS_PATH=${RIVET_ANALYSIS_DIR}
-Herwig++ run -N 25000 -s 1370${JOB_ID} --tag=`echo "_part${JOB_ID}"` LHC.run
+Sherpa -f Run.dat -j 2 -e 25000 -R 1370${JOB_ID} -a Rivet -A $OUTDIR
 
