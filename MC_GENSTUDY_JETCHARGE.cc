13aaaf2a8acc28ec043630c33816a10f19a17eb1
David Bjergaard <dbjergaard@gmail.com>
2013-01-15 11:01:36 -0500

    1. Changed indent style to 1TBS style http://en.wikipedia.org/wiki/Indent_style#Variant:_1TBS
    2. Changed (successfully) to using a map of Histograms without
    crashing on finalization.
    
    3. General clean-up for running on cluster.  No profiling has been
    done, so I don't expect any sort of performance gains.

--- MC_GENSTUDY_JETCHARGE.cc
+++ MC_GENSTUDY_JETCHARGE.cc
@@ -1,6 +1,6 @@
 // -*- C++ -*-
 //System includes
-//#include <map>
+#include <map>
 #include <algorithm>
 
 //Rivet framework
@@ -19,6 +19,7 @@
 #include "LWH/Histogram1D.h"
 #include "LWH/Histogram2D.h"
 
+typedef std::map<std::string,AIDA::IHistogram1D*> BookedHistos;
 namespace Rivet {
 
   /// Generic analysis looking at various distributions of final state particles
@@ -37,7 +38,6 @@ namespace Rivet {
     //@{
     /// Book histograms and initialise projections before the run
     void init() {
-
       // Projections
       const FinalState fs;
       addProjection(fs, "FS");
@@ -46,71 +46,58 @@ namespace Rivet {
       WFinder muWFinder(fs, muonEtaRanges, 25*GeV, MUON, 
 			40*GeV,1000*GeV,25*GeV,0.6,true,false,80.4,true);
 
-
       // Tag a W in the event, focus on jets that don't come from the W decay.
       addProjection(muWFinder,"muWFinder");
 
       FastJets JetProjection(muWFinder.remainingFinalState(),FastJets::ANTIKT, 0.6); //FastJets::KT,0.7
       addProjection(JetProjection,"Jets");
-
+      ///////////////
       // Histograms
-
-      _histJetMult		= bookHistogram1D("JetMult", 6, -0.5, 5.5);
+      ///////////////
+      _histograms["JetMult"]		= bookHistogram1D("JetMult"		, 6, -0.5, 5.5);
       //Jet Kinematics
-      _histJetPt		= bookHistogram1D("JetPt"	, 50, 33, 300);
-      _histJetE			= bookHistogram1D("JetE"	, 25, 20, 300);
-      _histJetEta		= bookHistogram1D("JetEta"	, 25, -2, 2);
-      _histJetRapidity		= bookHistogram1D("JetRapidity"	, 25, -2, 2);
-      //_histJetPhi		= bookHistogram1D("JetPhi"	, 25, 0, TWOPI);
-      _histJetMass		= bookHistogram1D("JetMass"	, 100, 0, 40);
-      //_histJet2Mass		= bookHistogram1D("Jet2Mass"	, 100, 0, 100);
-      //_histJet3Mass		= bookHistogram1D("Jet3Mass"	, 100, 0, 100);
-      _histSubJetMult		= bookHistogram1D("SubJetMult", 15, -0.5, 29.5);
-      /*
-	_histSubJetSkew		= bookHistogram1D("SubJetSkew", 25, 0, 0.1); 
-	_histSubJetStddev	= bookHistogram1D("SubJetStddev", 25, 0, 0.2); 
-      */
-      _histSubJet2Mass		= bookHistogram1D("SubJet2Mass"	, 100, 0, 35);
-      _histSubJet3Mass		= bookHistogram1D("SubJet3Mass"	, 100, 0, 45);
-      _histSubJetDeltaR		= bookHistogram1D("SubJetDeltaR", 50, 0, 1.0);
-      _histSubJetMass		= bookHistogram1D("SubJetMass"	, 100, 0, 12);
-      _histSubJetSumEt		= bookHistogram1D("SubJetSumEt", 30, 0, 175);
+      _histograms["JetPt"]		= bookHistogram1D("JetPt"		, 50, 33, 300);
+      _histograms["JetE"]		= bookHistogram1D("JetE"		, 25, 20, 300);
+      _histograms["JetEta"]		= bookHistogram1D("JetEta"		, 25, -2, 2);
+      _histograms["JetRapidity"]	= bookHistogram1D("JetRapidity"		, 25, -2, 2);
+      //_histograms["JetPhi"]		= bookHistogram1D("JetPhi"		, 25, 0, TWOPI);
+      _histograms["JetMass"]		= bookHistogram1D("JetMass"		, 100, 0, 40);
+      //_histograms["Jet2Mass"]		= bookHistogram1D("Jet2Mass"		, 100, 0, 100);
+      //_histograms["Jet3Mass"]		= bookHistogram1D("Jet3Mass"		, 100, 0, 100);
+      _histograms["SubJetMult"]		= bookHistogram1D("SubJetMult"		, 15, -0.5, 29.5);
+      _histograms["SubJet2Mass"]	= bookHistogram1D("SubJet2Mass"		, 100, 0, 35);
+      _histograms["SubJet3Mass"]	= bookHistogram1D("SubJet3Mass"		, 100, 0, 45);
+      _histograms["SubJetDeltaR"]	= bookHistogram1D("SubJetDeltaR"	, 50, 0, 1.0);
+      _histograms["SubJetMass"]		= bookHistogram1D("SubJetMass"		, 100, 0, 12);
+      _histograms["SubJetSumEt"]	= bookHistogram1D("SubJetSumEt"		, 30, 0, 175);
 
       //Jet Charge Histos
-      _histWJetCharge		= bookHistogram1D("WJetCharge", 50, -0.3, 0.3);
-      _histWCharge		= bookHistogram1D("WCharge", 3, -1.5, 1.5);
-      _hist2DJetChargeWPt       = bookHistogram2D("JetChargeVsWPt",50,-0.3,0.3,50,24,300);
-      _histJetPullTy             = bookHistogram1D("JetPullThetaTy",50,0,0.04);
+      _histograms["WJetCharge"]		= bookHistogram1D("WJetCharge"		, 50, -0.3, 0.3);
+      _histograms["WCharge"]		= bookHistogram1D("WCharge"		, 3, -1.5, 1.5);
+      _histograms["JetPullTheta"]       = bookHistogram1D("JetPullTheta"	,50,-PI,PI);
+      _histograms["JetPullMag"]         = bookHistogram1D("JetPullMag"          ,50,0,0.04);
+      _hist2DJetChargeWPt		= bookHistogram2D("JetChargeVsWPt"	,50,-0.3,0.3,50,24,300);
 
       //N-subjettiness histos	
-      
-      _histJetMassFilt		= bookHistogram1D("JetMassFilt", 60, 0, 50);
-      _histJetMassTrim		= bookHistogram1D("JetMassTrim", 60, 0, 50);
-      _histJetMassPrune		= bookHistogram1D("JetMassPrune", 60, 0, 20);
-      _histNSubJettiness	= bookHistogram1D("NSubJettiness", 40, -0.005, 1.005);
-      _histNSubJettiness1Iter	= bookHistogram1D("NSubJettiness1Iter", 40, -0.005, 1.005);
-      _histNSubJettiness2Iter	= bookHistogram1D("NSubJettiness2Iter", 40, -0.005, 1.005);
-
+      _histograms["JetMassFilt"]	= bookHistogram1D("JetMassFilt"		, 60, 0, 50);
+      _histograms["JetMassTrim"]	= bookHistogram1D("JetMassTrim"		, 60, 0, 50);
+      _histograms["JetMassPrune"]	= bookHistogram1D("JetMassPrune"	, 60, 0, 20);
+      _histograms["NSubJettiness"]	= bookHistogram1D("NSubJettiness"	, 40, -0.005, 1.005);
+      _histograms["NSubJettiness1Iter"]	= bookHistogram1D("NSubJettiness1Iter"	, 40, -0.005, 1.005);
+      _histograms["NSubJettiness2Iter"]	= bookHistogram1D("NSubJettiness2Iter"	, 40, -0.005, 1.005);
     }
     /// quickly calculate standard deviation of pt distribution in jets
-    virtual void pt_stddev(const PseudoJets& jets, double& mean,double& stddev,const double N)
-    {
+    virtual void pt_stddev(const PseudoJets& jets, double& mean,double& stddev,const double N) {
       foreach(const fastjet::PseudoJet& jet, jets)
-	  mean+=jet.pt();
+	mean+=jet.pt();
       mean=mean/N;
-      
       foreach(const fastjet::PseudoJet& jet, jets)
-	  stddev+=((jet.pt()-mean)*(jet.pt()-mean));
+	stddev+=((jet.pt()-mean)*(jet.pt()-mean));
       stddev=stddev/N;
     }
 
-    virtual void analyzeSubJets(const fastjet::PseudoJet& jet,const double weight)
-    {
-      //double dcut=0.4;
-      //double ptstddev	= 0.0;
-      //double ptmean	= 0.0;
-      //double ptskew	= 0.0;
-      double ptmin=0.5*GeV;
+    virtual void analyzeSubJets(const fastjet::PseudoJet& jet,const double weight) {
+      const double ptmin=0.5*GeV;
       double sumEt=0.0;
       fastjet::ClusterSequence clusterSeq(jet.validated_cs()->constituents(jet),fastjet::JetDefinition(fastjet::kt_algorithm,0.6)); 
 
@@ -119,41 +106,24 @@ namespace Rivet {
       fastjet::ClusterSequence antiKTClusterSeq(jet.validated_cs()->constituents(jet),fastjet::JetDefinition(fastjet::antikt_algorithm,0.1));
       PseudoJets smallSubJets=antiKTClusterSeq.inclusive_jets(ptmin);
       int smallJetMult = smallSubJets.size();
-      _histSubJetMult->fill(smallJetMult,weight);
-      /*
-      if(smallJetMult > 1)
-	{
-	  pt_stddev(smallSubJets,ptmean,ptstddev,static_cast<double>(smallJetMult));
-
-	  foreach(const fastjet::PseudoJet& jeta, smallSubJets)
-	    ptskew+=pow(jeta.pt()-ptmean,3);
-	  ptskew=(ptskew/(static_cast<double>(smallJetMult)))/(pow(ptstddev,1.5)*jet.pt());
-	  ptstddev=ptstddev/jet.pt();
-	}
-      _histSubJetSkew->fill(ptskew,weight);
-      _histSubJetStddev->fill(ptskew,weight);
-      */
+      _histograms["SubJetMult"]->fill(smallJetMult,weight);
       unsigned int nSubJets=subJets.size();
 
       if(nSubJets==3)
-	_histSubJet3Mass->fill((subJets.at(0)+subJets.at(1)+subJets.at(2)).m(),weight);
+	_histograms["SubJet3Mass"]->fill((subJets.at(0)+subJets.at(1)+subJets.at(2)).m(),weight);
       
-      for(unsigned int j=0;j!=nSubJets;++j)
-	{
-	  sumEt+=subJets.at(j).Et();
-	  _histSubJetMass->fill(subJets.at(j).m());
-	  for(unsigned int k=(j+1); k!=nSubJets;++k)
-	    {
-	      _histSubJetDeltaR->fill(subJets.at(j).delta_R(subJets.at(k)),weight);
-	      _histSubJet2Mass->fill((subJets.at(j)+subJets.at(k)).m(),weight);
-	    }
+      for(unsigned int j=0;j!=nSubJets;++j) {
+	sumEt+=subJets.at(j).Et();
+	_histograms["SubJetMass"]->fill(subJets.at(j).m());
+	for(unsigned int k=(j+1); k!=nSubJets;++k) {
+	  _histograms["SubJetDeltaR"]->fill(subJets.at(j).delta_R(subJets.at(k)),weight);
+	  _histograms["SubJet2Mass"]->fill((subJets.at(j)+subJets.at(k)).m(),weight);
 	}
-      _histSubJetSumEt->fill(sumEt,weight);
-
+      }
+      _histograms["SubJetSumEt"]->fill(sumEt,weight);
     }
     /// Perform the per-event analysis
-    void analyze(const Event& event) 
-    {
+    void analyze(const Event& event) {
       _nPassing[0]++;
       const WFinder& muWFinder = applyProjection<WFinder>(event,"muWFinder");
       if (muWFinder.bosons().size() != 1)
@@ -164,162 +134,83 @@ namespace Rivet {
       const FastJets& JetProjection = applyProjection<FastJets>(event, "Jets");
       const PseudoJets& jets = JetProjection.pseudoJetsByPt(35.0*GeV);
 
-      if (jets.size() > 0) 
-	{
-	  _nPassing[2]++;
-	  unsigned int jetMult=jets.size();
-	  _histJetMult->fill(jetMult);
-	  /// Rather than loop over all jets, just take the first hard
-	  /// one, Make sure entire jet is within fiducial volume
-	  if(jets.front().eta() > -(2.5-0.6) && jets.front().eta() < (2.5-0.6))
-	    {
-	      if(jets.front().has_valid_cs())
-		analyzeSubJets(jets.front(),weight);
+      if (jets.size() > 0) {
+	_nPassing[2]++;
+	unsigned int jetMult=jets.size();
+	_histograms["JetMult"]->fill(jetMult);
+	/// Rather than loop over all jets, just take the first hard
+	/// one, Make sure entire jet is within fiducial volume
+	if(jets.front().eta() > -(2.5-0.6) && jets.front().eta() < (2.5-0.6)) {
+	  if(jets.front().has_valid_cs())
+	    analyzeSubJets(jets.front(),weight);
 
-	      foreach (const fastjet::PseudoJet& jet, jets) 
-		{
-		  _histJetMassFilt->fill(JetProjection.Filter(jet, FastJets::CAM, 3, 0.3).m(), weight);
-		  _histJetMassTrim->fill(JetProjection.Trimmer(jet, FastJets::CAM, 0.03, 0.3).m(), weight);
-		  _histJetMassPrune->fill(JetProjection.Pruner(jet, FastJets::CAM, 0.4, 0.1).m(), weight);
-		  PseudoJets constituents = jet.constituents();
-		  if (constituents.size() > 10) 
-		    {
-		      //todo slurp into one function call
+	  foreach (const fastjet::PseudoJet& jet, jets) {
+	    _histograms["JetMassFilt"]->fill(JetProjection.Filter(jet, FastJets::CAM, 3, 0.3).m(), weight);
+	    _histograms["JetMassTrim"]->fill(JetProjection.Trimmer(jet, FastJets::CAM, 0.03, 0.3).m(), weight);
+	    _histograms["JetMassPrune"]->fill(JetProjection.Pruner(jet, FastJets::CAM, 0.4, 0.1).m(), weight);
+	    PseudoJets constituents = jet.constituents();
+	    if (constituents.size() > 10) {
+	      //todo slurp into one function call
 		      
-		      PseudoJets axes(JetProjection.GetAxes(2, constituents, FastJets::CAM, 0.5));
-		      double tau = JetProjection.TauValue(2, 1, constituents, axes);
-		      _histNSubJettiness->fill(tau, weight);
-		      JetProjection.UpdateAxes(2, constituents, axes);
-		      double tau1 = JetProjection.TauValue(2, 1, constituents, axes);
-		      JetProjection.UpdateAxes(2, constituents, axes);
-		      double tau2 = JetProjection.TauValue(2, 1, constituents, axes);
-		      _histNSubJettiness1Iter->fill(tau1, weight);
-		      _histNSubJettiness2Iter->fill(tau2, weight);
-		    }
-		}
-	      _nPassing[3]++;
-	      _histJetMass->fill(jets.front().m(),weight);
-	      _histJetPt->fill(jets.front().pt(),weight);	
-	      _histJetE->fill(jets.front().E(),weight);
-	      _histJetEta->fill(jets.front().eta(),weight);	
-	      _histJetRapidity->fill(jets.front().rapidity(),weight); 
-	      //histJetPhi->fill(jets.front().phi(),weight);	
-	      double wCharge=PID::charge(muWFinder.bosons().front().pdgId())+0.0;//dirty cast 
-	      double jetCharge=wCharge*JetProjection.JetCharge(jets.front(),0.5,1*GeV);
-	      std::pair<double,double> tvec=JetProjection.JetPull(jets.front());
-	      //std::cout<<"mag: "<<tvec.first<<"theta: "<<tvec.second<<endl;
-	      _hist2DJetChargeWPt->fill(jetCharge,muWFinder.bosons().front().momentum().pT(),weight);
-	      _histWJetCharge->fill(jetCharge,weight);
-	      _histWCharge->fill(wCharge,weight);
-	      _histJetPullTy->fill(tvec.first*cos(tvec.second),weight);
-	    }	      
-
-	}
-      else
+	      PseudoJets axes(JetProjection.GetAxes(2, constituents, FastJets::CAM, 0.5));
+	      double tau = JetProjection.TauValue(2, 1, constituents, axes);
+	      _histograms["NSubJettiness"]->fill(tau, weight);
+	      JetProjection.UpdateAxes(2, constituents, axes);
+	      double tau1 = JetProjection.TauValue(2, 1, constituents, axes);
+	      JetProjection.UpdateAxes(2, constituents, axes);
+	      double tau2 = JetProjection.TauValue(2, 1, constituents, axes);
+	      _histograms["NSubJettiness1Iter"]->fill(tau1, weight);
+	      _histograms["NSubJettiness2Iter"]->fill(tau2, weight);
+	    }
+	  }
+	  _nPassing[3]++;
+	  _histograms["JetMass"]->fill(jets.front().m(),weight);
+	  _histograms["JetPt"]->fill(jets.front().pt(),weight);	
+	  _histograms["JetE"]->fill(jets.front().E(),weight);
+	  _histograms["JetEta"]->fill(jets.front().eta(),weight);	
+	  _histograms["JetRapidity"]->fill(jets.front().rapidity(),weight); 
+	  //histograms["JetPhi"]->fill(jets.front().phi(),weight);	
+	  double wCharge=PID::charge(muWFinder.bosons().front().pdgId())+0.0;//dirty cast 
+	  double jetCharge=wCharge*JetProjection.JetCharge(jets.front(),0.5,1*GeV);
+	  std::pair<double,double> tvec=JetProjection.JetPull(jets.front());
+	  //std::cout<<"mag: "<<tvec.first<<"theta: "<<tvec.second<<endl;
+	  _hist2DJetChargeWPt->fill(jetCharge,muWFinder.bosons().front().momentum().pT(),weight);
+	  _histograms["WJetCharge"]->fill(jetCharge,weight);
+	  _histograms["WCharge"]->fill(wCharge,weight);
+	  _histograms["JetPullTheta"]->fill(tvec.first,weight);
+	  _histograms["JetPullMag"]->fill(tvec.second,weight);
+	}	      
+      }
+      else {
 	vetoEvent;
-
+      }
     }
     /// Finalize
-    void finalize() 
-    {
-      /*
-      //Why oh why can't I use a std::map<AIDA::IHistogram1D*>
-      foreach(BookedHistos::value_type H,Histograms)
-	scale(H.second,1/sumOfWeights());
-      */
+    void finalize() {
       cout<<"Cut summary: "<<endl;
       cout<<"| Inclusive | "<<_nPassing[0]<< " | "<<endl;
       cout<<"| Found W   | "<<_nPassing[1]<< " | "<<endl;
       cout<<"| >1 Jet    | "<<_nPassing[2]<< " | "<<endl;
       cout<<"| Fiducial  | "<<_nPassing[3]<< " | "<<endl;
-      
-      cout<<"Mean Jet Charge: "<<_histWJetCharge->mean()<<" +/- "<<_histWJetCharge->rms()<<endl;
-      
-      normalize(_histJetMult);
-      normalize(_histJetPt);
-      normalize(_histJetE);
-      normalize(_histJetEta);
-      normalize(_histJetRapidity);
-      //normalize(_histJetPhi);
-      normalize(_histJetMass);
+      cout<<"Mean Jet Charge: "<<_histograms["WJetCharge"]->mean()<<" +/- "<<_histograms["WJetCharge"]->rms()<<endl;
 
-      //normalize(_histJet2Mass);
-      //normalize(_histJet3Mass );
-
-      normalize(_histSubJet2Mass);
-      normalize(_histSubJet3Mass);
-      normalize(_histSubJetDeltaR);
-      normalize(_histSubJetMass);
-      normalize(_histSubJetSumEt);
-      //normalize(_histSubJetSkew);
-      //normalize(_histSubJetStddev);
-      //normalize(_histJetDipolarity,1.0);
-      normalize(_histWJetCharge);
-      normalize(_histWCharge);
+      foreach(BookedHistos::value_type H,_histograms)
+	normalize(H.second);
       normalize(_hist2DJetChargeWPt);
-      normalize(_histJetPullTy);
-      normalize(_histSubJetMult);
-      normalize(_histJetMassFilt);
-      normalize(_histJetMassTrim);
-      normalize(_histJetMassPrune);
-      normalize(_histNSubJettiness);
-      normalize(_histNSubJettiness1Iter);
-      normalize(_histNSubJettiness2Iter);
-
     }
     //@}
   private:
 
-    ///@param Histograms Indexed by histogram name for easy management
+    ///@param _histograms Indexed by histogram name for easy management
     ///until Rivet Autobooking takes over, allows any number of
     ///histograms to be added "on the fly" in the init() method.
     //@{
-    AIDA::IHistogram1D *_histJetMult;
-
-    //Jet Kinematics	
-    AIDA::IHistogram1D *_histJetPt;
-    AIDA::IHistogram1D *_histJetE;
-    AIDA::IHistogram1D *_histJetEta;
-    AIDA::IHistogram1D *_histJetRapidity;
-    //AIDA::IHistogram1D *_histJetPhi;
-    AIDA::IHistogram1D *_histJetMass;
-
-    // Jet theoreticals
-    //AIDA::IHistogram1D *_histJet2Mass;
-    //AIDA::IHistogram1D *_histJet3Mass;
-
-    //AIDA::IHistogram1D *_histSubJetSkew;
-    //AIDA::IHistogram1D *_histSubJetStddev;
-
-    AIDA::IHistogram1D *_histSubJet2Mass;
-    AIDA::IHistogram1D *_histSubJet3Mass;
-    AIDA::IHistogram1D *_histSubJetDeltaR;
-    AIDA::IHistogram1D *_histSubJetMass;
-    AIDA::IHistogram1D *_histSubJetSumEt;
-
-    //Jet Charge
-    AIDA::IHistogram1D *_histWJetCharge;    
-    AIDA::IHistogram1D *_histWCharge;
-    AIDA::IHistogram1D *_histSubJetMult;
+    BookedHistos _histograms;
     AIDA::IHistogram2D *_hist2DJetChargeWPt;
-    AIDA::IHistogram1D *_histJetPullTy;
-      //N-subjettiness histos	
-    AIDA::IHistogram1D *_histJetMassFilt;
-    AIDA::IHistogram1D *_histJetMassTrim;	
-    AIDA::IHistogram1D *_histJetMassPrune;		
-    AIDA::IHistogram1D *_histNSubJettiness;	
-    AIDA::IHistogram1D *_histNSubJettiness1Iter;	
-    AIDA::IHistogram1D *_histNSubJettiness2Iter;	
     //@}
     // Event count for efficiency study
     int _nPassing[4];
   };
-
-
   // The hook for the plugin system
   DECLARE_RIVET_PLUGIN(MC_GENSTUDY_JETCHARGE);
-
 }
-/// Garbage bin
-/*
- */
