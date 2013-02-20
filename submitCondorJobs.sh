3e9364d57ab7a94e77ffdf6ff4b8da312efdc031
David Bjergaard <dbjergaard@gmail.com>
2013-02-20 11:03:59 -0500

    finished tweaking submit script

--- submitCondorJobs.sh
+++ submitCondorJobs.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 #backup a sample of the previous job's output
 timestamp=`stat -c%Z ~/logs/rivet/Pythia6`
+echo -n "Backing up sample of logs from "; date -d @${timestamp}
 for f in `ls -lah ~/logs/rivet/{Pythia{6,8},Sherpa,Herwig++}/*.0 | grep '[0-9\.]*G'`
 do
     echo "File larger than 1GB" > f
 done
-tar cvf ${timestamp}_sample_output.tar ~/logs/rivet/{Pythia{6,8},Sherpa,Herwig++}/*.0
+tar cf ${timestamp}_sample_output.tar ~/logs/rivet/{Pythia{6,8},Sherpa,Herwig++}/*.0
 gzip ${timestamp}_sample_output.tar
-
+echo "Done"
+echo "Cleaning up old logs and aida files..."
+rm -f ~/logs/rivet/{Pythia{6,8},Sherpa,Herwig++}/*
+rm -rf ~/rivet/Analysis/rivet-jet-charge/HighStatsAida/{Pythia{6,8},Sherpa}/*
+rm -f ~/rivet/Analysis/rivet-jet-charge/MonteCarloParams/Herwig++/*_part*
+echo "Done"
 #submit
-# for f in CondorScripts/{Herwig++,Pythia{6,8},Sherpa}.job
-# do 
-#     condor_submit $f; 
-# done
-
+echo "Submitting new jobs"
+for f in CondorScripts/{Herwig++,Pythia{6,8},Sherpa}.job
+do 
+    condor_submit $f; 
+done
+echo "Done"
