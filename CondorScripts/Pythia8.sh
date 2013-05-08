1e3cf9cebf7c2b66fe39b1e50d1d8e954d233945
David Bjergaard <dbjergaard@gmail.com>
2013-05-08 14:34:56 -0400

    Updated pythia8 scripts for new runPythia.C

--- CondorScripts/Pythia8.sh
+++ CondorScripts/Pythia8.sh
@@ -12,7 +12,7 @@ source ${RIVET_PREFIX}/agileenv.sh
 cd ${RIVET_ANALYSIS_DIR}/MonteCarloParams/Pythia8/
 FIFONAME=/tmp/pythia8_fifo_${JOB_ID}.hepmc
 mkfifo ${FIFONAME}
-cat wJetProd.cnf | sed "s/seed = 1234/seed = 1370${JOB_ID}/g" > wJetProd${JOB_ID}.cnf
-cat wJetProd.cnf | sed "s/pp = 10/pp = ${TUNE}/g" > wJetProd${JOB_ID}.cnf
-./runPythia wJetProd${JOB_ID}.cnf ${FIFONAME} &
+#cat wJetProd.cnf | sed "s/seed = 1234/seed = 1370${JOB_ID}/g" > wJetProd${JOB_ID}.cnf
+#cat wJetProd.cnf | sed "s/pp = 10/pp = ${TUNE}/g" > wJetProd${JOB_ID}.cnf
+./runPythia wJetProd.cnf ${FIFONAME} 1370${JOB_ID} ${TUNE} &
 rivet --analysis-path=${RIVET_ANALYSIS_DIR} -a MC_GENSTUDY_JETCHARGE --histo-file=${OUTDIR}/Pythia8_tune${TUNE}_part${JOB_ID}.aida ${FIFONAME}
--- submitCondorJobs.sh
+++ submitCondorJobs.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 #backup a sample of the previous job's output
-timestamp=`stat -c%Z ~/logs/rivet/Pythia6`
+timestamp=`stat -c%Z ~/logs/rivet/Pythia8`
 echo -n "Backing up sample of logs from "; date -d @${timestamp}
-for f in `ls -lah ~/logs/rivet/{Pythia{6,8},Sherpa,Herwig++}/*.0 | grep '[0-9\.]*G'`
+for f in `ls -lah ~/logs/rivet/{Pythia8,Sherpa}/*.0 | grep '[0-9\.]*G'`
 do
     echo "File larger than 1GB" > f
 done
-tar cf ${timestamp}_sample_output.tar ~/logs/rivet/{Pythia{6,8},Sherpa,Herwig++}/*.0
+tar cf ${timestamp}_sample_output.tar ~/logs/rivet/{Pythia8,Sherpa}/*.0
 gzip ${timestamp}_sample_output.tar
 echo "Done."
 echo "Cleaning up old logs and aida files..."
-rm -f ~/logs/rivet/{Pythia{6,8},Sherpa,Herwig++}/*
-rm -rf ~/rivet/Analysis/rivet-jet-charge/HighStatsAida/{Pythia{6,8},Sherpa}/*
-rm -f ~/rivet/Analysis/rivet-jet-charge/MonteCarloParams/Herwig++/*_part*
+rm -f ~/logs/rivet/{Pythia8,Sherpa}/*
+rm -rf ~/rivet/Analysis/rivet-jet-charge/HighStatsAida/{Pythia8,Sherpa}/*
+#rm -f ~/rivet/Analysis/rivet-jet-charge/MonteCarloParams/Herwig++/*_part*
 echo "Done."
 #submit
 echo "Updating to latest analysis code"
 cd ~/rivet/Analysis/rivet-jet-charge
 git pull
 source ../../rivetenv.sh
-rivet-buildplugin RivetMC_GENSTUDY_JETCHARGE.so MC_GENSTUDY_JETCHARGE.cc
+make clean && make 
 echo "Done."
-for f in CondorScripts/{Herwig++,Pythia{6,8},Sherpa}.job
+for f in CondorScripts/{Pythia8,Sherpa}.job
 do 
     echo $f
     condor_submit $f; 
